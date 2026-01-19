f45d6e3b18d0c11179dda52c98b7c187fe5f99d4
EdyIordache19 <ediiordache18@gmail.com>
2026-01-19 23:35:31 +0200

    Made orders be generated of various types

--- src/orders_generator.cpp
+++ src/orders_generator.cpp
@@ -9,6 +9,7 @@ Order OrdersGenerator::generateRandomOrder(uint64_t id) {
     order.price = 90 + rand() % 21; // Random price between 90 and 110
     order.quantity = 1 + rand() % 100; // Random quantity between 1 and 100
     order.side = (rand() % 2 == 0) ? Order::BUY : Order::SELL;
+    order.tif = static_cast<TimeInForce>(rand() % 3);
     return order;
 }
 
@@ -23,7 +24,8 @@ std::list<Order> OrdersGenerator::generateOrdersToFile(const std::string& filena
     for (size_t i = 0; i < numOrders; ++i) {
         Order order = generateRandomOrder(i + 1);
         outFile << order.id << " " << order.price << " " << order.quantity << " "
-                << ((order.side == Order::BUY) ? "BUY" : "SELL") << "\n";
+                << ((order.side == Order::BUY) ? "BUY" : "SELL") << " "
+                << static_cast<int>(order.tif) << "\n";
         generatedOrders.push_back(order);
     }
 
