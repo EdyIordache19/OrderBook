b51d871be26762da55673c99960c9de9337e9b79
EdyIordache19 <ediiordache18@gmail.com>
2026-04-21 12:19:07 +0300

    Commented and cleaned ring_buffer.hpp

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
-#include "order_types.hpp"
-
 #include <vector>
 #include <atomic>
 
+/**
+ * @brief Lock-free SPSC ring buffer class for handoff between two threads
+ * @pre buff_size is a power of 2
+ *  - alignas and padding for CPU cache line friendliness
+ *  - atomic head and tail variables used with acquire/release for ordering in other threads
+ */
 template <typename T>
 class RingBuffer {
 private:
@@ -21,24 +25,28 @@ public:
         head = 0;
     }
 
+    // Producer writes buffer[slot] and stores in tail with release
     int push(const T& item) {
         if (tail - head.load(std::memory_order_acquire) >= buff_size) {
             return 1;
         }
 
-        std::atomic<size_t> slot = tail & (buff_size - 1);
+        // Bitwise operation
+        size_t slot = tail.load(std::memory_order_acquire) & (buff_size - 1);
         buffer[slot] = item;
 
-        tail.store(tail + 1, std::memory_order_release);
+        tail.store(tail + 1, std::memory_order_acquire);
         return 0;
     }
 
+    // Consumer reads buffer[slot] and stores in head with release
     int pop(T& item) {
         if (tail.load(std::memory_order_acquire) == head) {
             return 1;
         }
 
-        std::atomic<size_t> slot = head & (buff_size - 1);
+        // Bitwise operation
+        size_t slot = head.load(std::memory_order_acquire) & (buff_size - 1);
         item = buffer[slot];
 
         head.store(head + 1, std::memory_order_release);
