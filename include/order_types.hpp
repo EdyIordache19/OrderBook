e05e5cdab87073c5ff26c6e02ecba1e3fe0e5d2f
EdyIordache19 <ediiordache18@gmail.com>
2026-04-21 10:46:13 +0300

    Commented order_types.hpp

--- include/order_types.hpp
+++ include/order_types.hpp
@@ -1,17 +1,34 @@
+/**
+ * @file order_types.hpp
+ * @brief Header file for different structures, organizing orders and trades
+ * Price/Qty are represented as integers, instead of doubles, for determinism and speed
+ */
+
 #pragma once
 
 #include <cstdint>
 
+/**
+ * @brief Enums for different attributes
+ * Numeric values are for network protocol stability, don't change without updating all consumers
+ */
 enum OrderType : uint8_t {
     LIMIT = 0,
     MARKET = 1,
     CANCEL = 2,
+
+    // Kill order type sent from client to kill the engine
     KILL = 99
 };
 
 enum TimeInForce : uint8_t {
+    // Resting order
     GTC = 0, // Good 'till Cancel
+
+    // The remainder of the order not filled gets canceled
     IOC = 1, // Immediate or Cancel
+
+    // Cancel if not filled fully
     FOK = 2 // Fill or Kill
 };
 
@@ -20,6 +37,7 @@ enum Side : uint8_t {
     SELL = 1
 };
 
+// Used for defining type of messages sent through network, in MsgHdr
 enum MsgType : uint8_t {
     MSG_CANDLE = 1,
     MSG_BOOK_SNAPSHOT = 2,
@@ -27,6 +45,10 @@ enum MsgType : uint8_t {
     MSG_ACCOUNT_INFO = 4
 };
 
+/**
+ * @brief Wire format / Network payload
+ * __attribute__((packed)) chosen for stability on the wire
+ */
 struct __attribute__((packed)) PriceLevel {
     uint64_t price;
     uint32_t quantity;
@@ -42,6 +64,8 @@ struct __attribute__((packed)) PriceLevel {
 struct __attribute__((packed)) BookSnapshot {
     uint8_t num_bids;
     uint8_t num_asks;
+
+    // 10 Price levels for UI
     PriceLevel bids[10];
     PriceLevel asks[10];
 };
@@ -49,16 +73,23 @@ struct __attribute__((packed)) BookSnapshot {
 struct __attribute__((packed)) Candle {
     uint64_t open = 0;
     uint64_t high = 0;
+
+    // low initialized to UINT64_MAX, as first trade always sets it
     uint64_t low = -1;
     uint64_t close = 0;
     uint32_t volume = 0;
+
+    // is_active field for keeping track if current candle is open or not
     bool is_active = false;
     uint64_t simulated_time;
 };
 
 struct __attribute__((packed)) Trade {
+    // The ids of the maker/taker orders
     uint64_t maker_id;
     uint64_t taker_id;
+
+    // The ids of the maker/taker users making the orders
     uint64_t maker_user_id;
     uint64_t taker_user_id;
 
@@ -79,6 +110,42 @@ struct __attribute__((packed)) Trade {
         { }
 };
 
+struct __attribute__((packed)) AccountInfo {
+    int64_t usd;
+    int64_t equity;
+};
+
+struct __attribute__((packed)) MsgHdr {
+    MsgType type;
+
+    // Size of payload of packet
+    uint16_t size;
+};
+
+struct __attribute__((packed)) CandlePacket {
+    MsgHdr header;
+    Candle payload;
+};
+
+struct __attribute__((packed)) SnapshotPacket {
+    MsgHdr header;
+    BookSnapshot payload;
+};
+
+struct __attribute__((packed)) TradePacket {
+    MsgHdr header;
+    Trade payload;
+};
+
+struct __attribute__((packed)) AccountPacket {
+    MsgHdr header;
+    AccountInfo payload;
+};
+
+
+/**
+ * @brief In memory for engine logic
+ */
 struct alignas(64) Order {
     uint64_t id;
     uint32_t user_id;
@@ -90,6 +157,7 @@ struct alignas(64) Order {
     OrderType type;
     TimeInForce tif;
 
+    // next/prev only valid when resting on the orderbook
     Order *next = nullptr;
     Order *prev = nullptr;
 
@@ -113,32 +181,3 @@ struct Level {
     Order *tail = nullptr;
 };
 
-struct __attribute__((packed)) AccountInfo {
-    int64_t usd;
-    int64_t equity;
-};
-
-struct __attribute__((packed)) MsgHdr {
-    MsgType type;
-    uint16_t size;
-};
-
-struct __attribute__((packed)) CandlePacket {
-    MsgHdr header;
-    Candle payload;
-};
-
-struct __attribute__((packed)) SnapshotPacket {
-    MsgHdr header;
-    BookSnapshot payload;
-};
-
-struct __attribute__((packed)) TradePacket {
-    MsgHdr header;
-    Trade payload;
-};
-
-struct __attribute__((packed)) AccountPacket {
-    MsgHdr header;
-    AccountInfo payload;
-};
